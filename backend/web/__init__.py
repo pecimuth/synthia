ff7b1c3363063ab12382f2f19b602bbe65756c95
Andrej Pečimúth <andrejx98@gmail.com>
2021-02-17 22:06:51 +0100

    deployment

--- backend/web/__init__.py
+++ backend/web/__init__.py
@@ -8,21 +8,23 @@ import os
 
 
 def create_app() -> Flask:
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask(
+        __name__,
+        instance_relative_config=True
+    )
     app.config.from_mapping(
         SWAGGER={
             'title': 'Synthia'
         },
-        SECRET_KEY=os.environ['SECRET_KEY'],
-        EXTERN_DB_PATH=os.path.join(app.instance_path, 'extern/'),
+        SECRET_KEY=os.environ.get('SECRET_KEY'),
         PROJECT_STORAGE=os.path.join(app.instance_path, 'project'),
         DATABASE_DRIVER='postgresql',
-        DATABASE_USER=os.environ['POSTGRES_USER'],
-        DATABASE_PASSWORD=os.environ['POSTGRES_PASSWORD'],
-        DATABASE_DB=os.environ['POSTGRES_DB'],
-        DATABASE_HOST=os.environ['DATABASE_HOST'],
-        DATABASE_PORT=os.environ['DATABASE_PORT'],
-        ORIGIN=os.environ['ORIGIN']
+        DATABASE_USER=os.environ.get('POSTGRES_USER'),
+        DATABASE_PASSWORD=os.environ.get('POSTGRES_PASSWORD'),
+        DATABASE_DB=os.environ.get('POSTGRES_DB'),
+        DATABASE_HOST=os.environ.get('DATABASE_HOST'),
+        DATABASE_PORT=os.environ.get('DATABASE_PORT'),
+        ORIGIN=os.environ.get('ORIGIN')
     )
 
     Swagger(app)
