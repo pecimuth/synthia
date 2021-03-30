5a83ca6640d738a61b52a21fbac0353d5e3cdc27
Andrej Pečimúth <andrejx98@gmail.com>
2021-03-30 23:11:44 +0200

    fix generator assignment

--- backend/core/service/column_generator/assignment.py
+++ backend/core/service/column_generator/assignment.py
@@ -49,8 +49,8 @@ class GeneratorAssignment:
         factory = facade.get_generator_type()
         if not cls._assignment_allowed(factory, generator_setting, meta_column):
             return False
-        column_gen = facade.make_generator_instance()
-        if isinstance(column_gen, MultiColumnGenerator):
+        if issubclass(factory, MultiColumnGenerator):
+            column_gen = factory(generator_setting)
             column_gen.unite_with(meta_column)
         else:
             meta_column.generator_setting = generator_setting
